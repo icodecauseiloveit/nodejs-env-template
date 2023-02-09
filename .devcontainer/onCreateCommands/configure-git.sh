0e6421c9a0a6ae7088fce9e80b35a947a9e76d6a
Gary Baldrich Pinedo <46449103+icodecauseiloveit@users.noreply.github.com>
2023-02-08 22:08:52 -0500

    Adding git superlog alias as a visual view

--- .devcontainer/onCreateCommands/configure-git.sh
+++ .devcontainer/onCreateCommands/configure-git.sh
@@ -5,5 +5,7 @@ git config --global user.name "$NAME"
 git config --global user.email $EMAIL
 git config --global init.defaultBranch main
 git config --global core.editor "code --wait"
+git config --global alias.superlog "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+
 
 echo "Git configured successfully!"
